87dc26cce0508848fdf61e911fb93fa9f71af4a7
Jaskirat Singh <jsmokha33@gmail.com>
2022-01-10 12:37:30 +0530

    UI Structuring of StickyText and Images done

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let pencilToolCont = document.querySelector(".pencil-tool-cont");
 let eraserToolCont = document.querySelector(".eraser-tool-cont");
 let pencilimg = document.querySelector(".pencil");
 let eraserimg = document.querySelector(".eraser");
+let sticky=document.querySelector(".sticky");
+let upload = document.querySelector(".upload");
 let toolflag=true;
 let pencilflag=false;
 let eraserflag=false;
@@ -51,3 +53,95 @@ eraserimg.addEventListener("click",function(){
         eraserToolCont.style.display="none";
     }
 });
+sticky.addEventListener("click", (e) => {
+    let stickyTemplateHTML = `
+    <div class="header-cont">
+        <div class="minimize"></div>
+        <div class="remove"></div>
+    </div>
+    <div class="note-cont">
+        <textarea spellcheck="false"></textarea>
+    </div>
+    `;
+
+    createSticky(stickyTemplateHTML);
+})
+
+function createSticky(stickyTemplateHTML) {
+    let stickyCont = document.createElement("div");
+    stickyCont.setAttribute("class", "sticky-cont");
+    stickyCont.innerHTML = stickyTemplateHTML;
+    document.body.appendChild(stickyCont);
+
+    let minimize = stickyCont.querySelector(".minimize");
+    let remove = stickyCont.querySelector(".remove");
+    noteActions(minimize, remove, stickyCont);
+
+    stickyCont.onmousedown = function (event) {
+        dragAndDrop(stickyCont, event);
+    };
+
+    stickyCont.ondragstart = function () {
+        return false;
+    };
+}
+
+function noteActions(minimize, remove, stickyCont) {
+    remove.addEventListener("click", (e) => {
+        stickyCont.remove();
+    })
+    minimize.addEventListener("click", (e) => {
+        let noteCont = stickyCont.querySelector(".note-cont");
+        let display = getComputedStyle(noteCont).getPropertyValue("display");
+        if (display === "none") noteCont.style.display = "block";
+        else noteCont.style.display = "none";
+    })
+}
+function dragAndDrop(element, event) {
+    let shiftX = event.clientX - element.getBoundingClientRect().left;
+    let shiftY = event.clientY - element.getBoundingClientRect().top;
+
+    element.style.position = 'absolute';
+    element.style.zIndex = 1000;
+
+    moveAt(event.pageX, event.pageY);
+
+    function moveAt(pageX, pageY) {
+        element.style.left = pageX - shiftX + 'px';
+        element.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    element.onmouseup = function () {
+        document.removeEventListener('mousemove', onMouseMove);
+        element.onmouseup = null;
+    };
+}
+
+upload.addEventListener("click", (e) => {
+    // Open file explorer
+    let input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.click();
+
+    input.addEventListener("change", (e) => {
+        let file = input.files[0];
+        let url = URL.createObjectURL(file);
+
+        let stickyTemplateHTML = `
+        <div class="header-cont">
+            <div class="minimize"></div>
+            <div class="remove"></div>
+        </div>
+        <div class="note-cont">
+            <img src="${url}"/>
+        </div>
+        `;
+        createSticky(stickyTemplateHTML);
+    })
+})
